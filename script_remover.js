f968faece4a0cee6179aa0f1acca4febe49f395d
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-01-08 00:02:30 +0100

    Fixing script remover

--- script_remover.js
+++ script_remover.js
@@ -1,6 +1,6 @@
 export async function main(ns) {
 	if (await ns.prompt(`Do you want to remove scripts not in scripts.txt?`)) {
-		let scriptsToKeep = ns.read(listOfScripts).split('\n');
+		let scriptsToKeep = ns.read('scripts.txt').split('\n');
 		let scriptsToRemove = ns.ls('home').filter(file =>
 			(file.endsWith('.js') || file.endsWith('.txt')) &&
 			!scriptsToKeep.includes(file)
