53245e2de027298f076907f2230acc69b2b2297a
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-01-08 23:46:29 +0100

    Formatting

--- cost_25_pservs.js
+++ cost_25_pservs.js
@@ -1,5 +1,5 @@
 export async function main(ns) {
 	let power = ns.args[0];
 	let cost = ns.getPurchasedServerCost(Math.pow(2, power));
-	ns.tprint(ns.nFormat(cost, "0.000a") + " / " + ns.nFormat(cost * 25, "0.000a"));
+	ns.tprint(ns.nFormat(cost, '0.000a') + ' / ' + ns.nFormat(cost * 25, '0.000a'));
 }
