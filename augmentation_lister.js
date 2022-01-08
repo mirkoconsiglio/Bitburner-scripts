dfdc88856101df1647bb2534b97c53ac90d790d7
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-01-08 12:35:03 +0100

    Added augmentation lister

---
+++ augmentation_lister.js
@@ -0,0 +1,18 @@
+import {getFactions} from 'utils.js';
+
+export async function main(ns) {
+	let factions = getFactions();
+	await ns.write('augmentations_list.txt', '', 'w');
+	for (let faction of factions) {
+		await ns.write('augmentations_list.txt', `\n\n----- ${faction} -----\n`, 'a');
+		let augmentations = ns.getAugmentationsFromFaction(faction);
+		for (let augmentation of augmentations) {
+			if (augmentation !== 'NeuroFlux Governor') {
+				await ns.write('augmentations_list.txt', `\n--- ${augmentation} ---\n`, 'a');
+				for (let [stat, multiplier] of Object.entries(ns.getAugmentationStats(augmentation))) {
+					await ns.write('augmentations_list.txt', `${stat}: ${multiplier}\n`, 'a');
+				}
+			}
+		}
+	}
+}
