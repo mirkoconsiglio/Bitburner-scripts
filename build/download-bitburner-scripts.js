15f4574828a156d478f0e582eb7d2fcad0fa3d94
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-01-11 00:51:28 +0100

    updated downloader

--- build/download-bitburner-scripts.js
+++ build/download-bitburner-scripts.js
@@ -1,17 +1,18 @@
-const url = 'https://raw.githubusercontent.com/mirkoconsiglio/Bitburner-scripts/master';
-const listOfScripts = `build/scripts.txt`;
-
 export async function main(ns) {
+	const url = 'https://raw.githubusercontent.com/mirkoconsiglio/Bitburner-scripts/master';
+	const listOfScripts = `build/scripts.txt`;
 	ns.tprint('----- Downloading scripts -----');
 	try {
 		ns.tprint(`Downloading ${listOfScripts}`);
-		let download = await ns.wget(`${url}/build/${listOfScripts}`, listOfScripts);
+		let download = await ns.wget(`${url}/${listOfScripts}`, '/' + listOfScripts);
 		if (!download) throw listOfScripts;
 		let scripts = ns.read(listOfScripts).split('\n');
+		ns.tprint(scripts);
+		ns.exit();
 		for (let script of scripts) {
 			ns.tprint(`Downloading ${script}`);
 			if (script.includes('/')) script = '/' + script;
-			download = await ns.wget(`${url}/${script}`, script);
+			download = await ns.wget(`${url}${script}`, script);
 			if (!download) throw script;
 		}
 		ns.tprint('----- Download complete -----');
--- build/scripts.js
+++ build/scripts.js
@@ -22,7 +22,7 @@ function getFilesRecursive(dir, arrayOfFiles) {
 function getFiles(dir = __dirname) {
 	let files = getFilesRecursive(`${dir}`);
 	let relativeFiles = [];
-	files.forEach(file => relativeFiles.push('/' + path.relative(dir, file).replace(/\\/g, '/')));
+	files.forEach(file => relativeFiles.push(path.relative(dir, file).replace(/\\/g, '/')));
 	return relativeFiles;
 }
 
