ad0140d2fd94b1debf97fb51cb8b7897359e0f0a
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-01-16 12:27:41 +0100

    fixed exp generator

--- hacking/exp-generator.js
+++ hacking/exp-generator.js
@@ -10,15 +10,16 @@ import {
 export async function main(ns) {
 	await copyScriptsToAll(ns);
 	const threads = 1000;
+	let i = 0;
 	while (true) {
 		let scripts = getScripts();
 		let servers = getAccessibleServers(ns);
 		let hackables = getOptimalHackable(ns, servers);
 		let [freeRams, filteredHackables] = getFreeRam(ns, servers, hackables, true);
-
 		for (let target of filteredHackables.reverse()) {
-			findPlaceToRun(ns, scripts.grow, threads, freeRams, [target]);
-			findPlaceToRun(ns, scripts.weaken, threads, freeRams, [target]);
+			findPlaceToRun(ns, scripts.grow, threads, freeRams, [target, 0, i]);
+			findPlaceToRun(ns, scripts.weaken, threads, freeRams, [target, 0, i]);
+			i++;
 		}
 		await ns.sleep(1000);
 	}
