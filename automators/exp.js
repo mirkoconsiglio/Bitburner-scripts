a889d5ed8e63616e9e38f7c3fc74208bf02634b5
Mirko <mirkoconsiglio@hotmail.co.uk>
2022-01-21 12:18:34 +0100

    moved to automators

--- automators/exp.js
+++ automators/exp.js
@@ -5,14 +5,14 @@ import {
 	getFreeRam,
 	getOptimalHackable,
 	getScripts
-} from "utils/utils.js";
+} from '/utils/utils.js';
 
 export async function main(ns) {
 	await copyScriptsToAll(ns);
-	const threads = 1000;
+	const threads = Number.MAX_VALUE;
+	const scripts = getScripts()
 	let i = 0;
 	while (true) {
-		let scripts = getScripts();
 		let servers = getAccessibleServers(ns);
 		let hackables = getOptimalHackable(ns, servers);
 		let [freeRams, filteredHackables] = getFreeRam(ns, servers, hackables, true);
