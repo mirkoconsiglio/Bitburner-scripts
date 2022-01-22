09088a53e9ba7ea18643babf2abb541019d45055
Mirko <mirkoconsiglio@hotmail.co.uk>
2022-01-22 17:48:13 +0100

    fixed crime

--- automators/crime.js
+++ automators/crime.js
@@ -2,7 +2,7 @@ export async function main(ns) {
 	const inc = 100;
 	while (!ns.isBusy()) {
 		const duration = ns.commitCrime(ns.args[0]);
-		for (let i = 0; i < duration; i += inc) {
+		for (let i = 0; i < duration - 5 * inc; i += inc) {
 			await ns.sleep(inc);
 			if (!ns.isBusy()) return;
 		}
