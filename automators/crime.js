5e7f5316a5dfd347025891f8abb512b83945526d
Mirko <mirkoconsiglio@hotmail.co.uk>
2022-01-23 19:50:40 +0100

    FINALLY FIXED AUTOCRIME

--- automators/crime.js
+++ automators/crime.js
@@ -1,11 +1,8 @@
 export async function main(ns) {
-	const inc = 100;
-	while (!ns.isBusy()) {
-		const duration = ns.commitCrime(ns.args[0]);
-		for (let i = 0; i < duration - 5 * inc; i += inc) {
-			await ns.sleep(inc);
-			if (!ns.isBusy()) return;
-		}
-		await ns.sleep(10 * inc);
+	const crime = ns.args[0];
+	const interval = 100;
+	while (true) {
+		ns.tail(); // Necessary to exit script
+		await ns.sleep(ns.commitCrime(crime) + interval);
 	}
 }
