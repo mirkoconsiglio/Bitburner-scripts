396cc6d0d176163fa9af30eb36bf073d45338d41
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-01-26 20:14:28 +0100

    exp automator always targets joesguns

--- automators/exp.js
+++ automators/exp.js
@@ -1,26 +1,15 @@
-import {
-	copyScriptsToAll,
-	findPlaceToRun,
-	getAccessibleServers,
-	getFreeRam,
-	getOptimalHackable,
-	getScripts
-} from '/utils/utils.js';
+import {copyScriptsToAll, findPlaceToRun, getAccessibleServers, getFreeRam, getScripts} from '/utils/utils.js';
 
 export async function main(ns) {
 	await copyScriptsToAll(ns);
 	const threads = Number.MAX_VALUE;
-	const scripts = getScripts()
+	const scripts = getScripts();
 	let i = 0;
 	while (true) {
 		let servers = getAccessibleServers(ns);
-		let hackables = getOptimalHackable(ns, servers);
-		let [freeRams, filteredHackables] = getFreeRam(ns, servers, hackables, true);
-		for (let target of filteredHackables.reverse()) {
-			findPlaceToRun(ns, scripts.grow, threads, freeRams, [target, 0, i]);
-			findPlaceToRun(ns, scripts.weaken, threads, freeRams, [target, 0, i]);
-			i++;
-		}
+		let freeRams = getFreeRam(ns, servers);
+		findPlaceToRun(ns, scripts.grow, threads, freeRams, ['joesguns', 0, i]);
+		findPlaceToRun(ns, scripts.weaken, threads, freeRams, ['joesguns', 0, i]);
 		await ns.sleep(1000);
 	}
 }
