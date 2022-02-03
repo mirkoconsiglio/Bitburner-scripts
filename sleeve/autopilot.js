6ca5ccdcabcdc29b063dd775c2f7354fbbd77354
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-02-03 19:28:20 +0100

    update to include syncing

--- sleeve/autopilot.js
+++ sleeve/autopilot.js
@@ -2,14 +2,17 @@ export async function main(ns) {
 	ns.disableLog('ALL');
 	while (true) {
 		ns.clearLog();
-		const task = (ns.heart.break() > -54e3) ? 'crime' : 'shock-recovery';
 		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
 			const sleeveStats = ns.sleeve.getSleeveStats(i);
-			if (task === 'crime' || sleeveStats.shock === 0) {
-				const crime = sleeveStats.strength < 100 ? 'Mug' : 'Homicide';
+			if (sleeveStats.sync < 100) {
+				if (ns.sleeve.getTask(i).task !== 'Synchronize') ns.sleeve.setToSynchronize(i);
+			} else if (ns.heart.break() > -54e3 || sleeveStats.shock === 0) {
+				const crime = sleeveStats.strength < 50 ? 'Mug' : 'Homicide';
 				if (ns.sleeve.getTask(i).crime !== crime) ns.sleeve.setToCommitCrime(i, crime);
-			} else if (ns.sleeve.getTask(i).task !== 'Shock Recovery') ns.sleeve.setToShockRecovery(i);
-			ns.print(ns.sleeve.getTask(i));
+			} else {
+				if (ns.sleeve.getTask(i).task !== 'Shock Recovery') ns.sleeve.setToShockRecovery(i);
+			}
+			ns.print(`Sleeve ${i}: ${ns.sleeve.getTask(i)}`);
 		}
 		await ns.sleep(1000);
 	}
