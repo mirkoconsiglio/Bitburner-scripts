4fb563996080d4eb34514e40c63d56a4b949797b
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-02-09 23:03:58 +0100

    added sleeve aug buying

--- sleeve/autopilot.js
+++ sleeve/autopilot.js
@@ -1,15 +1,24 @@
-// TODO: Augmentation purchasing
+import {isUsefulCombat} from '/augmentations/utils.js';
+
 export async function main(ns) {
 	ns.disableLog('ALL');
 	while (true) {
 		ns.clearLog();
 		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
+			// Check for augmentation purchases
+			const augmentations = ns.sleeve.getSleevePurchasableAugs(i);
+			for (let [cost, name] of augmentations) {
+				if (isUsefulCombat(ns, name) && ns.getServerMoneyAvailable('home') >= cost) {
+					ns.sleeve.purchaseSleeveAug(i, name);
+				}
+			}
+			// Assign task
 			const sleeveStats = ns.sleeve.getSleeveStats(i);
-			if (sleeveStats.sync < 100) {
-				if (ns.sleeve.getTask(i).task !== 'Synchronize') ns.sleeve.setToSynchronize(i);
-			} else if (ns.heart.break() > -54e3 || sleeveStats.shock === 0) {
+			if (ns.heart.break() > -54e3 || (sleeveStats.shock === 0 && sleeveStats.sync === 0)) {
 				const crime = sleeveStats.strength < 50 ? 'Mug' : 'Homicide';
 				if (ns.sleeve.getTask(i).crime !== crime) ns.sleeve.setToCommitCrime(i, crime);
+			} else if (sleeveStats.sync < 100) {
+				if (ns.sleeve.getTask(i).task !== 'Synchronize') ns.sleeve.setToSynchronize(i);
 			} else {
 				if (ns.sleeve.getTask(i).task !== 'Shock Recovery') ns.sleeve.setToShockRecovery(i);
 			}
