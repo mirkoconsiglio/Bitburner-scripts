c9ebe2a32ba7c4df95202a04298c9d6ad1a4d663
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-02-16 00:33:00 +0100

    updated sleeve autopilot

--- sleeve/autopilot.js
+++ sleeve/autopilot.js
@@ -4,18 +4,31 @@ export async function main(ns) {
 	ns.disableLog('ALL');
 	while (true) {
 		ns.clearLog();
+		const player = ns.getPlayer();
 		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
-			// Check for augmentation purchases
+			// Check for augmentation purchases (only crime)
 			const augmentations = ns.sleeve.getSleevePurchasableAugs(i);
 			for (let aug of augmentations) {
 				if (isUsefulCrime(ns, aug.name) && ns.getServerMoneyAvailable('home') >= aug.cost) {
 					ns.sleeve.purchaseSleeveAug(i, aug.name);
 				}
 			}
-			// Assign crime
-			const crime = ns.sleeve.getSleeveStats(i).strength < 50 ? 'Mug' : 'Homicide';
-			if (ns.sleeve.getTask(i).crime !== crime) ns.sleeve.setToCommitCrime(i, crime);
-			ns.print(`Sleeve ${i}: ${ns.sleeve.getTask(i).crime}`);
+			// Assign tasks
+			if (i === 0 && player.isWorking && player.workType === 'Working for Faction') { // Sleeve 0 copies player
+				const name = player.currentWorkFactionName;
+				const workType = player.workType;
+				if (ns.sleeve.getTask(i).task !== 'Working for Faction' ||
+					ns.sleeve.getTask(i).factionWorkType !== workType) ns.sleeve.setToFactionWork(i, name, workType);
+				ns.print(`Sleeve ${i}: ${ns.sleeve.getTask(i).factionWorkType}`);
+			} else if (i === 0 && player.isWorking && player.workType === 'Working for Company') { // Sleeve 0 copies player
+				const name = player.currentWorkFactionName;
+				if (ns.sleeve.getTask(i).task !== 'Working for Company') ns.sleeve.setToCompanyWork(i, name);
+				ns.print(`Sleeve ${i}: ${ns.sleeve.getTask(i).factionWorkType}`);
+			} else { // Crime
+				const crime = ns.sleeve.getSleeveStats(i).strength < 50 ? 'Mug' : 'Homicide';
+				if (ns.sleeve.getTask(i).crime !== crime) ns.sleeve.setToCommitCrime(i, crime);
+				ns.print(`Sleeve ${i}: ${ns.sleeve.getTask(i).crime}`);
+			}
 		}
 		await ns.sleep(1000);
 	}
