7dcfeba61cae56f887ff309b68320d01b80ab0eb
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-02-23 12:40:29 +0100

    fixed deploy daemons bug

--- hacking/deploy-daemons.js
+++ hacking/deploy-daemons.js
@@ -10,14 +10,14 @@ export function deployDaemons(ns) {
 	const hackables = getOptimalHackable(ns, servers);
 	const minimumRam = 2 ** 14; // 16 TiB
 	// filter and sort servers according to RAM
-	servers.filter(server => ns.getServerMaxRam(server) >= minimumRam).sort((a, b) => ns.getServerMaxRam(b) - ns.getServerMaxRam(a));
+	const hosts = servers.filter(server => ns.getServerMaxRam(server) >= minimumRam).sort((a, b) => ns.getServerMaxRam(b) - ns.getServerMaxRam(a));
 	// Deploy daemons
 	let c = 0;
-	for (let server of servers) {
+	for (let host of hosts) {
 		if (c === hackables.length) break; // If we run out of hackable servers break
-		if (!ns.isRunning(scripts.daemon, server, hackables[c])) {
-			ns.scriptKill(scripts.daemon, server);
-			ns.exec(scripts.daemon, server, 1, hackables[c]);
+		if (!ns.isRunning(scripts.daemon, host, hackables[c])) {
+			ns.scriptKill(scripts.daemon, host);
+			ns.exec(scripts.daemon, host, 1, hackables[c]);
 			c++;
 		}
 	}
