13a5a9a055e758e285d9a2701cc0c2b8ab090bed
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-02-26 14:32:55 +0100

    automated crime automator

--- automators/crime.js
+++ automators/crime.js
@@ -2,6 +2,6 @@ export async function main(ns) {
 	// noinspection InfiniteLoopJS
 	while (true) {
 		ns.tail(); // Necessary to exit script
-		await ns.sleep(ns.commitCrime(ns.args[0]) + 100);
+		await ns.sleep(ns.commitCrime(ns.args[0] ?? ns.getPlayer().strength < 50 ? 'Mug' : 'Homicide') + 100);
 	}
 }
