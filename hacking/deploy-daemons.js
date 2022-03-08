07797a68b84a681730e5fb8668970ea6d83809a0
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-03-08 17:50:14 +0100

    QOL

--- hacking/deploy-daemons.js
+++ hacking/deploy-daemons.js
@@ -4,11 +4,10 @@ export async function main(ns) {
 	deployDaemons(ns);
 }
 
-export function deployDaemons(ns) {
+export function deployDaemons(ns, minimumRam = 2 ** 14) {
 	const scripts = getScripts();
 	const servers = getAccessibleServers(ns);
 	const hackables = getOptimalHackable(ns, servers);
-	const minimumRam = 2 ** 14; // 16 TiB
 	// filter and sort servers according to RAM
 	const hosts = servers.filter(server => ns.getServerMaxRam(server) >= minimumRam).sort((a, b) => ns.getServerMaxRam(b) - ns.getServerMaxRam(a));
 	// Deploy daemons
