668946057f340bbefd15978ffae5f2ee486497f3
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-03-22 15:26:42 +0100

    forgot to add import statement

--- build/script-remover.js
+++ build/script-remover.js
@@ -1,3 +1,5 @@
+import {getFileHandle} from 'utils.js';
+
 /**
  *
  * @param {NS} ns
@@ -5,10 +7,8 @@
  */
 export async function main(ns) {
 	const scriptsToKeep = ['/build/scripts.txt', '/build/download-bitburner-scripts.js'];
-	ns.read('/build/scripts.txt').split('\n').forEach(script => {
-		if (script.includes('/')) scriptsToKeep.push('/' + script);
-		else scriptsToKeep.push(script);
-	});
+	ns.read('/build/scripts.txt').split('\n').forEach(script =>
+		script.includes('/') ? scriptsToKeep.push('/' + script) : scriptsToKeep.push(script));
 	for (let i = 1; i <= 20; i++) scriptsToKeep.push(getFileHandle(i));
 	let files = ns.ls('home').filter(file => file.endsWith('.js') || file.endsWith('.txt'));
 	if (files.length === 0) {
