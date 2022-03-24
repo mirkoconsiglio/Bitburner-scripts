261b5e2c9c4b7985cddd06539f399789ea791a80
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-03-24 12:19:12 +0100

    changed intelligence farmer to use Insight's script

--- player/intelligence.js
+++ player/intelligence.js
@@ -1,4 +1,20 @@
-import {getScripts} from '/utils.js';
+import {getCities} from '/utils.js';
+
+const argsSchema = [
+	['trips-per-cycle', 1e4],
+	['money-threshold', 1e12]
+];
+
+/**
+ *
+ * @param {*} data
+ * @param {*} args
+ * @returns {*[]}
+ */
+export function autocomplete(data, args) {
+	data.flags(argsSchema);
+	return [];
+}
 
 /**
  *
@@ -6,14 +22,45 @@ import {getScripts} from '/utils.js';
  * @returns {Promise<void>}
  */
 export async function main(ns) {
-	const args = ns.flags([
-		['moneyThreshold', 1e9],
-		['threads', 1],
-		['host', ns.getHostname()]
-	]);
-	const moneyThreshold = args.moneyThreshold;
-	const threads = args.threads;
-	const host = args.host;
-	const script = getScripts().intelligence;
-	for (let i = 0; i < threads; i++) ns.exec(script, host, 1, moneyThreshold, i);
+	ns.disableLog('ALL');
+	const options = ns.flags(argsSchema);
+	const tripsPerCycle = options['trips-per-cycle'];
+	const moneyThreshold = options['money-threshold'];
+	ns.print(`trips-per-cycle: ${tripsPerCycle}`);
+	ns.print(`money-threshold: ${ns.nFormat(moneyThreshold, '$0.000a')}`);
+	const cities = getCities();
+	const citiesLength = cities.length;
+	let justStarted = true;
+	let previousInt = ns.getPlayer().intelligence;
+	let currentInt = previousInt;
+	let previousLevelTime = Date.now();
+	let levelupTime;
+	let cycles = 0;
+	let duration = 0;
+	let tripsPerLevel = 0;
+	let tripsPerMs = 0;
+	// noinspection InfiniteLoopJS
+	while (true) {
+		while (ns.getPlayer().money > moneyThreshold) {
+			for (let i = 0; i < tripsPerCycle; i++) cities.forEach(city => ns.travelToCity(city));
+			await ns.sleep(1);
+			cycles++;
+			if (previousInt !== ns.getPlayer().intelligence) {
+				currentInt = ns.getPlayer().intelligence;
+				levelupTime = Date.now();
+				duration = levelupTime - previousLevelTime;
+				tripsPerLevel = cycles * tripsPerCycle * citiesLength;
+				tripsPerMs = Math.floor(tripsPerLevel / duration);
+				ns.print(`Level Up: Int ${currentInt}` + (justStarted ? ` partial ` : ` full `) +
+					`level in ${ns.tFormat(duration)} & ${ns.nFormat(tripsPerLevel, '0.000a')} travels`);
+				ns.print(`Approximately ${tripsPerMs} trips/ms`);
+				previousLevelTime = levelupTime;
+				previousInt = currentInt;
+				justStarted = false;
+				cycles = 0;
+			}
+		}
+		await ns.sleep(10000);
+		ns.print(`Below money threshold, waiting 10 seconds`);
+	}
 }
