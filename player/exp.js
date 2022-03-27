37b4c3d4a2e12d8f166312904ce3f1e3a95c3e1f
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-03-28 00:02:12 +0200

    fixed bug

--- player/exp.js
+++ player/exp.js
@@ -9,13 +9,18 @@ export async function main(ns) {
 	await copyScriptsToAll(ns);
 	const threads = Number.MAX_SAFE_INTEGER;
 	const scripts = getScripts();
+	const target = 'joesguns';
+	const level = ns.getServerRequiredHackingLevel(target);
+	while (ns.getPlayer().hacking < level) {
+		await ns.sleep(1000);
+	}
 	let i = 0;
 	// noinspection InfiniteLoopJS
 	while (true) {
 		const servers = getAccessibleServers(ns);
 		const freeRams = getFreeRam(ns, servers);
-		findPlaceToRun(ns, scripts.grow, threads, freeRams, 'joesguns', 0, i);
-		findPlaceToRun(ns, scripts.weaken, threads, freeRams, 'joesguns', 0, i);
+		findPlaceToRun(ns, scripts.grow, threads, freeRams, target, 0, i);
+		findPlaceToRun(ns, scripts.weaken, threads, freeRams, target, 0, i);
 		i++;
 		await ns.sleep(1000);
 	}
