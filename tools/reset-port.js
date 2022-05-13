a41f6ea6541d192d79b1d4071cca9601f9a9998c
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-05-13 13:51:16 +0200

    reset port update

--- tools/reset-port.js
+++ tools/reset-port.js
@@ -1,4 +1,4 @@
-import {initData, resetData} from '/utils.js';
+import {resetData} from '/utils.js';
 
 const argsSchema = [
 	['all', false]
@@ -17,7 +17,7 @@ export function autocomplete(data, args) {
  */
 export async function main(ns) {
 	const options = ns.flags(argsSchema);
-	ns.tprint(options._);
-	if (!options.all && options._.length === 0) throw new Error(`Must specify which port or --all`);
-	options.all ? await initData(ns) : await resetData(ns, options._[0]);
+	if (!options.all && options._.length === 0) throw new Error(`Must specify which port(s) or --all`);
+	if (options.all) for (let i = 1; i <= 20; i++) await resetData(ns, i);
+	else for (let i of options._) await resetData(ns, i);
 }
