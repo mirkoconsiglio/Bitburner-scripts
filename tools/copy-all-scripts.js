b57594a34cc02edd04e77b4943fc0223437e2294
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-05-19 17:29:16 +0200

    script to copy all scripts

---
+++ tools/copy-all-scripts.js
@@ -0,0 +1,13 @@
+export function autocomplete(data) {
+	return data.servers;
+}
+
+/**
+ *
+ * @param {NS} ns
+ * @returns {Promise<void>}
+ */
+export async function main(ns) {
+	for (const script of ns.read('/build/scripts.txt').split('\n'))
+		await ns.scp(script.includes('/') ? '/' + script : script, 'home', ns.args[0]);
+}
