4c14e8d24348882fd7af883479ab1222c3ce071a
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-05-24 18:19:56 +0200

    Shadows of Anarchy

--- augmentations/lister.js
+++ augmentations/lister.js
@@ -1,4 +1,4 @@
-import {formatMoney, getFactions} from '/utils.js';
+import {formatMoney, formatNumber, getFactions} from '/utils.js';
 
 /**
  *
@@ -11,20 +11,21 @@ export async function main(ns) {
 	await ns.write(file, '', 'w');
 	for (const faction of factions) {
 		await ns.write(file, `\n\n----- ${faction} -----\n`, 'a');
-		const augmentations = ns.getAugmentationsFromFaction(faction);
+		const augmentations = ns.singularity.getAugmentationsFromFaction(faction);
 		for (const augmentation of augmentations) {
 			if (augmentation !== 'NeuroFlux Governor') {
 				await ns.write(file, `\n--- ${augmentation} ---\n`, 'a');
-				const stats = Object.entries(ns.getAugmentationStats(augmentation));
+				const stats = Object.entries(ns.singularity.getAugmentationStats(augmentation));
 				if (stats.length > 0) for (const [stat, multiplier] of stats) await ns.write(file, `${stat}: ${multiplier}\n`, 'a');
 				else await ns.write(file, `Special\n`, 'a');
-				const price = ns.getAugmentationPrice(augmentation);
-				const repReq = ns.getAugmentationRepReq(augmentation);
-				const prereq = ns.getAugmentationPrereq(augmentation);
+				const price = ns.singularity.getAugmentationPrice(augmentation);
+				const repReq = ns.singularity.getAugmentationRepReq(augmentation);
+				const prereqs = ns.singularity.getAugmentationPrereq(augmentation);
 				await ns.write(file, `Price: ${formatMoney(ns, price)}\n`, 'a');
 				await ns.write(file, `Rep: ${formatNumber(ns, repReq)}\n`, 'a');
-				if (prereq.length > 0) await ns.write(file, `Prereq: ${prereq}\n`, 'a');
+				if (prereqs.length > 0) for (const prereq of prereqs) await ns.write(file, `Prereq: ${prereq}\n`, 'a');
 			}
 		}
+		await ns.sleep(1);
 	}
 }
