a8128d2f3a8c1af1f42089407a5dcb9b769a11a4
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-08-30 00:53:44 +0200

    updated crime automator

--- crime/automate.js
+++ crime/automate.js
@@ -5,16 +5,12 @@ export function autocomplete(data, options) {
 	return [...getCrimes()];
 }
 
-// TODO: Crime now autoloops so we need fix
 /**
  *
  * @param {NS} ns
  * @returns {Promise<void>}
  */
 export async function main(ns) {
-	// noinspection InfiniteLoopJS
-	while (true) {
-		ns.tail(); // Necessary to exit script
-		await ns.sleep(ns.singularity.commitCrime(ns.args[0] ?? ns.getPlayer().skills.strength < 50 ? 'mug' : 'homicide') + 100);
-	}
+	const crime = ns.args[0] ?? 'homicide';
+	ns.singularity.commitCrime(crime, ns.singularity.isFocused());
 }
